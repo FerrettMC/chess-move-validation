c56c040375519b48930ac3a573ce35be3cba27f4
FerrettMC <chester.t.ferret@gmail.com>
2026-02-10 18:38:16 -0600

    all logic moved

--- pieces.js
+++ pieces.js
@@ -1,4 +1,4 @@
-import { files } from "./validate.js";
+import { files, blackSquares, whiteSquares } from "./validate.js";
 export function pawn(from, to, color, board, toLetterIndex, fromLetterIndex) {
   const firstMoveSquares = color == "white" ? [4, 2] : [5, 7];
   const enPassantSquare = color == "white" ? 5 : 4;
@@ -110,57 +110,281 @@ export function pawn(from, to, color, board, toLetterIndex, fromLetterIndex) {
   };
 }
 
-export function knight(
-  from,
-  to,
-  piece,
-  color,
-  hasMoved,
-  board,
-  toLetterIndex,
-  fromLetterIndex,
-) {}
+export function rook(from, to, color, board, toLetterIndex, fromLetterIndex) {
+  // Not on board
 
-export function rook(
-  from,
-  to,
-  piece,
-  color,
-  hasMoved,
-  board,
-  toLetterIndex,
-  fromLetterIndex,
-) {}
+  if (to[1] !== from[1] && to[0] !== from[0]) {
+    return {
+      error: true,
+      newPosition: from,
+      message: `Cannot move to this position`,
+    };
+  }
+  const direction = to[1] === from[1] ? "horizontal" : "vertical";
+  switch (direction) {
+    case "horizontal":
+      for (let i = 1; i < Math.abs(toLetterIndex - fromLetterIndex); i++) {
+        let blockingPiece = false;
+        if (toLetterIndex > fromLetterIndex) {
+          blockingPiece = board.find(
+            (p) => p.position === `${files[fromLetterIndex + i]}${to[1]}`,
+          );
+        } else {
+          blockingPiece = board.find(
+            (p) => p.position === `${files[fromLetterIndex - i]}${to[1]}`,
+          );
+        }
+        if (blockingPiece) {
+          return {
+            error: true,
+            newPosition: from,
+            message: `Piece blocking you on ${blockingPiece.position}`,
+          };
+        }
+      }
+    case "vertical":
+      for (let i = 1; i < Math.abs(Number(to[1]) - Number(from[1])); i++) {
+        let blockingPiece = false;
+        if (Number(to[1]) > Number(from[1])) {
+          blockingPiece = board.find(
+            (p) => p.position === `${from[0]}${Number(from[1]) + i}`,
+          );
+        } else {
+          blockingPiece = board.find(
+            (p) => p.position === `${from[0]}${Number(from[1]) - i}`,
+          );
+        }
+        if (blockingPiece) {
+          return {
+            error: true,
+            newPosition: from,
+            message: `Piece blocking you on ${blockingPiece.position}`,
+          };
+        }
+      }
+  }
+  if (board.some((p) => p.position === to)) {
+    const otherPiece = board.find((p) => p.position === to);
+    if (otherPiece) {
+      if (otherPiece.color !== color) {
+        return {
+          newPosition: to,
+          message: `Took ${otherPiece.piece} on ${to}`,
+        };
+      } else {
+        return {
+          error: true,
+          newPosition: from,
+          message: `Cannot take own ${otherPiece.piece}`,
+        };
+      }
+    }
+  }
+  return { newPosition: to };
+}
 
-export function bishop(
-  from,
-  to,
-  piece,
-  color,
-  hasMoved,
-  board,
-  toLetterIndex,
-  fromLetterIndex,
-) {}
+export function bishop(from, to, color, board, toLetterIndex, fromLetterIndex) {
+  const right = fromLetterIndex < toLetterIndex ? true : false;
+  const up = Number(from[1]) < Number(to[1]) ? true : false;
+  if (
+    (blackSquares.includes(from) && !blackSquares.includes(to)) ||
+    (whiteSquares.includes(from) && !whiteSquares.includes(to))
+  ) {
+    return {
+      error: true,
+      newPosition: from,
+      message: `Cannot move like this`,
+    };
+  }
+  if (Math.abs(fromLetterIndex - toLetterIndex) !== Math.abs(from[1] - to[1])) {
+    return {
+      error: true,
+      newPosition: from,
+      message: `Cannot move like this`,
+    };
+  }
+  const dx = right ? 1 : -1;
+  const dy = up ? 1 : -1;
+
+  for (let i = 1; i < Math.abs(fromLetterIndex - toLetterIndex); i++) {
+    const numHorizontal = fromLetterIndex + dx * i;
+    const numVertical = Number(from[1]) + dy * i;
+
+    if (
+      board.some(
+        (p) =>
+          p.position[0] === files[numHorizontal] &&
+          Number(p.position[1]) === numVertical,
+      )
+    ) {
+      return {
+        error: true,
+        newPosition: from,
+        message: `Cannot move here, piece in the way`,
+      };
+    }
+  }
+  if (board.some((p) => p.position === to)) {
+    const otherPiece = board.find((p) => p.position === to);
+    if (otherPiece) {
+      if (otherPiece.color !== color) {
+        return {
+          newPosition: to,
+          message: `Took ${otherPiece.piece} on ${to}`,
+        };
+      } else {
+        return {
+          error: true,
+          newPosition: from,
+          message: `Cannot take own ${otherPiece.piece}`,
+        };
+      }
+    }
+  }
+  return { newPosition: to };
+}
+
+export function knight(from, to, color, board, toLetterIndex, fromLetterIndex) {
+  if (
+    Math.abs(toLetterIndex - fromLetterIndex) +
+      Math.abs(Number(to[1]) - Number(from[1])) !==
+    3
+  ) {
+    return {
+      error: true,
+      newPosition: from,
+      message: `Cannot move like this`,
+    };
+  }
+  if (board.some((p) => p.position === to)) {
+    const otherPiece = board.find((p) => p.position === to);
+    if (otherPiece) {
+      if (otherPiece.color !== color) {
+        return {
+          newPosition: to,
+          message: `Took ${otherPiece.piece} on ${to}`,
+        };
+      } else {
+        return {
+          error: true,
+          newPosition: from,
+          message: `Cannot take own ${otherPiece.piece}`,
+        };
+      }
+    }
+  }
+  return { newPosition: to };
+}
+
+export function queen(from, to, color, board, toLetterIndex, fromLetterIndex) {
+  const right = fromLetterIndex < toLetterIndex ? true : false;
+  const up = Number(from[1]) < Number(to[1]) ? true : false;
+  if (toLetterIndex !== fromLetterIndex && to[1] !== from[1]) {
+    return bishop(from, to, color, board, toLetterIndex, fromLetterIndex);
+  } else {
+    return rook(from, to, color, board, toLetterIndex, fromLetterIndex);
+  }
+}
 
 export function king(
   from,
   to,
-  piece,
   color,
-  hasMoved,
   board,
+  hasMoved,
   toLetterIndex,
   fromLetterIndex,
-) {}
+) {
+  if (Math.abs(fromLetterIndex - toLetterIndex) === 2 && from[1] === to[1]) {
+    if (hasMoved || from[0] !== "E") {
+      return {
+        error: true,
+        newPosition: from,
+        message: `Cannot move like this`,
+      };
+    }
+    const direction = fromLetterIndex - toLetterIndex === 2 ? "left" : "right";
+    switch (direction) {
+      case "left": {
+        const rook = board.find((p) => p.position === `A${from[1]}`);
+        if (!rook || rook.hasMoved || rook.color !== color) {
+          return {
+            error: true,
+            newPosition: from,
+            message: `No rook to castle with`,
+          };
+        }
 
-export function queen(
-  from,
-  to,
-  piece,
-  color,
-  hasMoved,
-  board,
-  toLetterIndex,
-  fromLetterIndex,
-) {}
+        for (let i = 1; i <= 3; i++) {
+          if (board.some((p) => p.position === `${files[i]}${from[1]}`)) {
+            return {
+              error: true,
+              newPosition: from,
+              message: `Piece in the way`,
+            };
+          }
+        }
+        return { newPosition: to };
+      }
+      case "right": {
+        const rook = board.find((p) => p.position === `H${from[1]}`);
+        if (!rook || rook.hasMoved || rook.color !== color) {
+          return {
+            error: true,
+            newPosition: from,
+            message: `No rook to castle with`,
+          };
+        }
+        for (let i = 1; i <= 2; i++) {
+          if (board.some((p) => p.position === `${files[7 - i]}${from[1]}`)) {
+            return {
+              error: true,
+              newPosition: from,
+              message: `Piece in the way`,
+            };
+          }
+        }
+        return { newPosition: to };
+      }
+
+      default:
+        return {
+          error: true,
+          newPosition: from,
+          message: `Cannot move like this`,
+        };
+    }
+  }
+  if (
+    !(from[0] === to[0] && Math.abs(Number(to[1]) - Number(from[1])) === 1) &&
+    !(from[1] === to[1] && Math.abs(toLetterIndex - fromLetterIndex) === 1) &&
+    !(
+      Math.abs(toLetterIndex - fromLetterIndex) === 1 &&
+      Math.abs(Number(to[1]) - Number(from[1])) === 1
+    )
+  ) {
+    return {
+      error: true,
+      newPosition: from,
+      message: `Cannot move like this`,
+    };
+  }
+  if (board.some((p) => p.position === to)) {
+    const otherPiece = board.find((p) => p.position === to);
+    if (otherPiece) {
+      if (otherPiece.color !== color) {
+        return {
+          newPosition: to,
+          message: `Took ${otherPiece.piece} on ${to}`,
+        };
+      } else {
+        return {
+          error: true,
+          newPosition: from,
+          message: `Cannot take own ${otherPiece.piece}`,
+        };
+      }
+    }
+  }
+  return { newPosition: to };
+}
