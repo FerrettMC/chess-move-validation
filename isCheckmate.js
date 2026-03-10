7451b1e3ff127787156b61f370ac86bd5c2704a4
FerrettMC <chester.t.ferret@gmail.com>
2026-03-10 13:30:35 -0500

    added blocking checkmate logic

--- isCheckmate.js
+++ isCheckmate.js
@@ -1,6 +1,7 @@
 import { checkPrime } from "crypto";
 import { files, isPutInCheck } from "./isPutInCheck.js";
 import { pawn, knight, rook, bishop, king, queen } from "./pieces.js";
+import { dir } from "console";
 export async function isCheckMate(color, board, checkmatingPiece) {
   const theKing = board.find((p) => p.color === color && p.piece === "king");
   const possibleDirections = [
@@ -47,7 +48,7 @@ export async function isCheckMate(color, board, checkmatingPiece) {
       }
     }
   }
-  // Check if other pieces can block/take the opposing piece
+  // Check if other pieces can take the opposing piece
   let sameColorPieces = board.filter((p) => p.color === theKing.color);
 
   for (const samePiece in sameColorPieces) {
@@ -140,6 +141,215 @@ export async function isCheckMate(color, board, checkmatingPiece) {
     }
   }
 
+  // Check if other pieces can block the opposing piece
+  const path = getPath(checkmatingPiece, theKing, board);
+  for (const samePiece in sameColorPieces) {
+    for (const pos in path) {
+      const fromLetterIndex = files.findIndex(
+        (p) => p === samePiece.position[0],
+      );
+      const toLetterIndex = files.findIndex((p) => p === pos[0]);
+      switch (samePiece) {
+        case "pawn":
+          result = await pawn(
+            samePiece,
+            samePiece.position,
+            pos,
+            samePiece.color,
+            board,
+            toLetterIndex,
+            fromLetterIndex,
+            true,
+          );
+          break;
+
+        case "rook":
+          result = await rook(
+            samePiece,
+            samePiece.position,
+            pos,
+            samePiece.color,
+            board,
+            toLetterIndex,
+            fromLetterIndex,
+            true,
+          );
+          break;
+
+        case "bishop":
+          result = await bishop(
+            samePiece,
+            samePiece.position,
+            pos,
+            samePiece.color,
+            board,
+            toLetterIndex,
+            fromLetterIndex,
+            true,
+          );
+          break;
+
+        case "knight":
+          result = await knight(
+            samePiece,
+            samePiece.position,
+            pos,
+            samePiece.color,
+            board,
+            toLetterIndex,
+            fromLetterIndex,
+            true,
+          );
+          break;
+
+        case "king":
+          result = await king(
+            samePiece,
+            samePiece.position,
+            pos,
+            samePiece.color,
+            board,
+            samePiece.hasMoved,
+            toLetterIndex,
+            fromLetterIndex,
+            true,
+          );
+          break;
+
+        case "queen":
+          result = await queen(
+            samePiece,
+            samePiece.position,
+            pos,
+            samePiece.color,
+            board,
+            toLetterIndex,
+            fromLetterIndex,
+            true,
+          );
+          break;
+      }
+      if (!result.error) {
+        return { checkmate: false };
+      }
+    }
+  }
+
   // Finally, return checkmate is true if there are no legal moves
   return { checkmate: true };
+}
+
+function getPath(piece, king, board) {
+  let path = [];
+  const fromLetterIndex = files.findIndex((p) => p === piece.position[0]);
+  const toLetterIndex = files.findIndex((p) => p === king.position[0]);
+  if (Math.abs(toLetterIndex - fromLetterIndex) === 1) {
+    return path;
+  }
+  const fromNumber = Number(piece.position[1]);
+  const toNumber = Number(king.position[1]);
+  switch (piece.piece) {
+    case "pawn":
+      return path;
+    case "knight":
+      return path;
+
+    case "bishop": {
+      return bishopPath(
+        fromLetterIndex,
+        toLetterIndex,
+        fromNumber,
+        toNumber,
+        path,
+      );
+    }
+
+    case "rook": {
+      return rookPath(
+        fromLetterIndex,
+        toLetterIndex,
+        fromNumber,
+        toNumber,
+        path,
+      );
+    }
+    case "queen": {
+      if (
+        Math.abs(fromLetterIndex - toLetterIndex) !== 0 &&
+        Math.abs(toNumber - fromNumber) !== 0
+      ) {
+        return bishopPath(
+          fromLetterIndex,
+          toLetterIndex,
+          fromNumber,
+          toNumber,
+          path,
+        );
+      } else {
+        return rookPath(
+          fromLetterIndex,
+          toLetterIndex,
+          fromNumber,
+          toNumber,
+          path,
+        );
+      }
+    }
+  }
+}
+
+function bishopPath(
+  fromLetterIndex,
+  toLetterIndex,
+  fromNumber,
+  toNumber,
+  path,
+) {
+  const dx = fromLetterIndex < toLetterIndex ? 1 : -1;
+  const dy = fromNumber < toNumber ? 1 : -1;
+  for (let i = 1; i < Math.abs(toLetterIndex - fromLetterIndex); i++) {
+    const letter = files[fromLetterIndex + i * dx];
+    const number = fromNumber + i * dy;
+    path.push(`${letter}${number}`);
+  }
+  return path;
+}
+
+function rookPath(fromLetterIndex, toLetterIndex, fromNumber, toNumber, path) {
+  let direction;
+  let dx;
+  let dy;
+  if (fromLetterIndex == toLetterIndex) {
+    direction = "vertical";
+  } else {
+    direction = "horizontal";
+  }
+  switch (direction) {
+    case "vertical": {
+      if (fromNumber < toNumber) {
+        dy = 1;
+      } else {
+        dy = -1;
+      }
+      for (let i = 1; i < Math.abs(toNumber - fromNumber); i++) {
+        const letter = files[fromLetterIndex];
+        const number = fromNumber + i * dy;
+        path.push(`${letter}${number}`);
+      }
+      return path;
+    }
+    case "horizontal": {
+      if (fromLetterIndex < toLetterIndex) {
+        dx = 1;
+      } else {
+        dx = -1;
+      }
+      for (let i = 1; i < Math.abs(toLetterIndex - fromLetterIndex); i++) {
+        const letter = files[fromLetterIndex + i * dx];
+        const number = fromNumber;
+        path.push(`${letter}${number}`);
+      }
+      return path;
+    }
+  }
 }
